50b0ab3bb86f2c5dd4fd58d4410886b0c8de251c
Jiayue Yu <jiy048@gmail.com>
2025-11-29 16:27:05 -0800

    add new use case in start_docker - build imgae after Dockerfile changes

--- start_docker.sh
+++ start_docker.sh
@@ -5,6 +5,12 @@ set -euo pipefail
 YML_PATH="docker-image.yml"
 BUILD_CONTEXT="."
 
+# define command variables 
+MODE="normal"
+if [[ "${1-}" == "--bump" ]]; then
+  MODE="bump"
+fi
+
 # find image name and tag in docker-image.yml
 IMAGE_LINE=$(grep -E '^\s*image:' "$YML_PATH" | head -n1 || true)
 echo "$IMAGE_LINE"
@@ -23,9 +29,29 @@ BASE_TAG=${IMAGE_REF#*:}      # 1.0.1
 echo "Config image from $YML_PATH:"
 echo "  IMAGE_NAME = $IMAGE_NAME"
 echo "  BASE_TAG   = $BASE_TAG"
+echo "  MODE         = $MODE"
+
+## if Dockerfile changes, build image with ./start_docker.sh --bump
+if [[ "$MODE" == "bump" ]]; then
+# find next version
+  IFS='.' read -r major minor patch <<< "$BASE_TAG" || true
+  patch=$((patch + 1))
+  NEXT_BASE_TAG="${major}.${minor}.${patch}"
+
+  TS=$(date +%Y%m%d%H%M%S)
+  FULL_TAG="${NEXT_BASE_TAG}_${TS}"
+  
+   echo "building image: ${IMAGE_NAME}:${FULL_TAG}"
+  docker build -t "${IMAGE_NAME}:${FULL_TAG}" "$BUILD_CONTEXT"
+  docker run -it --rm \
+  -p 8080:8080 \
+  "${IMAGE_NAME}:${FULL_TAG}"
+  exit 0
+fi
 
 #   find local images and check if anything matches base image
 EXISTING_TAGS=$(docker images "$IMAGE_NAME" --format '{{.Tag}}' | grep "^${BASE_TAG}_" || true)
+
 
 if [[ -z "$EXISTING_TAGS" ]]; then
   echo "No local image found for version '$BASE_TAG', need to build."
@@ -51,6 +77,6 @@ else
 
 fi
   
-  docker run -it --rm \
-    -p 8080:8080 \
-    "${IMAGE_NAME}:${FULL_TAG}"
+docker run -it --rm \
+  -p 8080:8080 \
+  "${IMAGE_NAME}:${FULL_TAG}"
