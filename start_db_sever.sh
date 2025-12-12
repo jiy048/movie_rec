5c0083540c9653d0c2e524d0090a1b01d83b33bb
Jiayue Yu <jiy048@gmail.com>
2025-12-11 16:20:49 -0800

    add docker compose

---
+++ start_db_sever.sh
@@ -0,0 +1,7 @@
+docker run -d \
+  --network movie_net \
+  -p 9200:9200 \
+  -e discovery.type=single-node \
+  -e xpack.security.enabled=false \
+  -e ES_JAVA_OPTS="-Xms512m -Xmx512m" \
+  docker.elastic.co/elasticsearch/elasticsearch:7.9.3
